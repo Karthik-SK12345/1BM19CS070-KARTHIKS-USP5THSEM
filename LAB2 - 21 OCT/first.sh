c0d6579f861a4ba820ea3202221d485ff851d2aa
Karthik S <71306768+Karthik-SK12345@users.noreply.github.com>
2021-11-11 12:54:10 +0530

    Add files via upload

---
+++ LAB2 - 21 OCT/first.sh
@@ -0,0 +1,8 @@
+echo "THis is my first USP lab"
+echo "Commands to be executed in this lab are:"
+date 
+cal
+ls -l
+pwd
+echo "Today's date is `date` "
+echo "There are `ls|wc` files in the `pwd` directory"
